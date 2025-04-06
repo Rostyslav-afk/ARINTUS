755de5ec9e9d2454ebda2aa4fc9d3e3dec4059cf
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-04-06 16:20:21 +0200

    full
    all css(adaption for mobile), js, html

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// MAIN BASE
+let mainArrayBase = JSON.parse(localStorage.getItem("mainArrayBase")) || [];
+
+// CONST
+const showArray = document.getElementById("show-array");
+const addToArray = document.getElementById("header-add");
+const showText = document.getElementById("header-show");
+
+// FUNCTIONS
+function showArrayy() {
+    showText.textContent = mainArrayBase.join(", "); // робимо гарніше виведення
+    console.log("Масив показано:", mainArrayBase);
+}
+
+function addToArrayy() {
+    const whatToAdd = prompt("Write your data:");
+    if (whatToAdd) {
+        mainArrayBase.push(whatToAdd);
+        localStorage.setItem("mainArrayBase", JSON.stringify(mainArrayBase)); // ⬅️ ДОДАЙ ЦЕ
+    }
+}
